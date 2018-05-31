c33661621b3a2569e63907cb8bede6d731bee980
suchita <suchitadarvekar@gmail.com>
2018-05-31 15:48:36 +0530

    brNCH CREATE

---
+++ GitDemo/testTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  testTableViewCell.swift
+//  GitDemo
+//
+//  Created by webwerks on 31/05/18.
+//  Copyright © 2018 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class testTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
